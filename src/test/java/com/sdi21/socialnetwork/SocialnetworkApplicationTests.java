df86aa4667e03bbc2de4da6a0c8be7e3f6a40156
Diego Martín Fernández <diegomartinfnz@gmail.com>
2022-03-09 13:39:07 +0100

    Creation project

---
+++ src/test/java/com/sdi21/socialnetwork/SocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdi21.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
