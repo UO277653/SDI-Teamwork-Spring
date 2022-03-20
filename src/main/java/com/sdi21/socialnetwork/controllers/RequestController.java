d382efd74d6b6bc8149a5708f0ffb26289497aa5
UO270119 <78871046+UO270119@users.noreply.github.com>
2022-03-20 19:57:41 +0100

    Accept button redirects correctly and pagination working

--- src/main/java/com/sdi21/socialnetwork/controllers/RequestController.java
+++ src/main/java/com/sdi21/socialnetwork/controllers/RequestController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +41,7 @@ public class RequestController {
         return "request/list";
     }
 
-    @PostMapping("/request/accept/{id}") //("/request/list")
+    @GetMapping("/request/accept/{id}") //("/request/list")
     public String acceptFriendRequest(Model model, @PathVariable Long id, Principal principal, Pageable pageable) {
         requestService.setFriendRequestAccepted(id);
         return getFriendRequestList(model, principal, pageable); //"request/list";
