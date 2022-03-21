fa506d43b40a3cd8824f9fae7f63be8b143e2379
uo276188 <uo276188@uniovi.es>
2022-03-21 19:49:03 +0100

    Faltan 23, 28, 29, 39, opcional

--- src/test/java/com/sdi21/socialnetwork/pageobjects/PO_LoginView.java
+++ src/test/java/com/sdi21/socialnetwork/pageobjects/PO_LoginView.java
@@ -1,5 +1,6 @@
 package com.sdi21.socialnetwork.pageobjects;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -33,5 +34,14 @@ public class PO_LoginView extends PO_NavView {
 
     static public void logout(WebDriver driver){
         PO_HomeView.clickOption(driver, "logout", "class", "btn btn-primary");
+    }
+
+    public static void checkLogin(WebDriver driver, int locale){
+        String checkText = PO_HomeView.getP().getString("login.message", locale);
+        List<WebElement> result = PO_View.checkElementBy(driver, "text", checkText);
+        Assertions.assertEquals(checkText, result.get(0).getText());
+        checkText = PO_HomeView.getP().getString("label.password", locale) + ":";
+        result = PO_View.checkElementBy(driver, "text", checkText);
+        Assertions.assertEquals(checkText, result.get(0).getText());
     }
 }
