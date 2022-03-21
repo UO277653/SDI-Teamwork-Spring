25789be7ba819ef96f1947b2af9fcdeb17a39a02
Stelian Adrian Stanci <UO277653@uniovi.es>
2022-03-21 18:04:41 +0100

    Arregle unos test, falta el 39

--- src/test/java/com/sdi21/socialnetwork/SocialnetworkApplicationTests.java
+++ src/test/java/com/sdi21/socialnetwork/SocialnetworkApplicationTests.java
@@ -24,7 +24,7 @@ class SocialnetworkApplicationTests {
 	//static String Geckodriver = "C:\\Users\\Alejandro\\Desktop\\SDI-2022\\software\\software\\geckodriver-v0.27.0-win64\\geckodriver.exe";
 
   	// Adrian
-  	//static String Geckodriver = "C:\\Users\\adria\\OneDrive\\Escritorio\\UNIVERSIDAD\\AÑO 3\\SEMESTRE 2\\Sistemas Distribuidos e Internet\\Laboratorio\\Lab5\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
+  	static String Geckodriver = "C:\\Users\\adria\\OneDrive\\Escritorio\\UNIVERSIDAD\\AÑO 3\\SEMESTRE 2\\Sistemas Distribuidos e Internet\\Laboratorio\\Lab5\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
 
 	//Sara
 	//static String Geckodriver = "D:\\UNI\\3º\\2º cuatri\\SDI\\Lab\\sesion05\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
@@ -33,7 +33,7 @@ class SocialnetworkApplicationTests {
 	//static String Geckodriver = "C:\\Users\\dimar\\Desktop\\sdi\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
 
 	//Ari
-	static String Geckodriver = "C:\\Users\\UO270119\\Desktop\\IIS (definitiva)\\3º - Tercero\\Segundo cuatri\\Sistemas Distribuidos e Internet\\Lab\\[materiales]\\5. Selenium\\PL-SDI-Sesión5-material\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
+	//static String Geckodriver = "C:\\Users\\UO270119\\Desktop\\IIS (definitiva)\\3º - Tercero\\Segundo cuatri\\Sistemas Distribuidos e Internet\\Lab\\[materiales]\\5. Selenium\\PL-SDI-Sesión5-material\\PL-SDI-Sesión5-material\\geckodriver-v0.30.0-win64.exe";
 
 	static WebDriver driver = getDriver(PathFirefox, Geckodriver);
 	static String URL = "http://localhost:8090";
@@ -731,16 +731,28 @@ class SocialnetworkApplicationTests {
 		PO_LoginView.fillLoginForm(driver, "nofriends@email.com", "123456");
 		driver.navigate().to("http://localhost:8090/logger/list");
 
-		// Se deberá indicar un mensaje de acción prohibida.
+		Assertions.assertTrue(driver.findElement(By.id("loginForm")).isDisplayed());
 	}
 
 	@Test
 	@Order(33)
 	public void Prueba33(){
 		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+		PO_LoginView.fillLoginForm(driver, "usuarioquenoexiste", "noexiste");
+		PO_LoginView.fillLoginForm(driver, "otroquenoexiste", "noexiste");
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		PO_SignUpView.fillForm(driver, "sarap2@uniovi.es", "Paco", "Perez", "123456", "123456");
+		PO_HomeView.clickOption(driver, "logout", "class", "btn btn-primary");
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		PO_SignUpView.fillForm(driver, "sarap3@uniovi.es", "Paco", "Perez", "123456", "123456");
+		PO_HomeView.clickOption(driver, "logout", "class", "btn btn-primary");
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
 		PO_LoginView.fillLoginForm(driver, "admin@email.com", "admin");
+		PO_HomeView.clickOption(driver, "logout", "class", "btn btn-primary");
+		PO_LoginView.fillLoginForm(driver, "admin@email.com", "admin");
+		driver.navigate().to("http://localhost:8090/logger/list");
+		Assertions.assertTrue(PO_LoggerView.verifyThatThereAreNOfLogMessages(driver, 2));
 
-		// Esta prueba deberá generar al menos dos interacciones de cada tipo y comprobar que el listado incluye los logs correspondientes.
 	}
 
 	@Test
