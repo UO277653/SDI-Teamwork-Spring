6b0b36810922d4dfccb2ebf30569d25e91e8b90f
Diego Martín Fernández <diegomartinfnz@gmail.com>
2022-03-21 17:04:30 +0100

    Logger

--- src/main/java/com/sdi21/socialnetwork/loghandlers/LogoutSuccessfulHandler.java
+++ src/main/java/com/sdi21/socialnetwork/loghandlers/LogoutSuccessfulHandler.java
@@ -21,6 +21,6 @@ public class LogoutSuccessfulHandler implements LogoutSuccessHandler {
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         loggerService.addLog(LogType.LOGOUT, "SUCCESSFUL LOGOUT: "+ authentication.getName());
-        response.sendRedirect(request.getContextPath());
+        response.sendRedirect("/login");
     }
 }
